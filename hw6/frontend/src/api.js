885833a6427fedf08f2ac563670ae919fcf2d1de
Welonbai <b07505036@ntu.edu.tw>
2023-01-07 02:04:51 +0800

    deploy 3

--- hw6/frontend/src/api.js
+++ hw6/frontend/src/api.js
@@ -11,7 +11,7 @@ import axios from "axios";
 const API_ROOT =
   process.env.NODE_ENV === "production"
     ? "/api"
-    : "http://localhost:4000/api";
+    : "http://localhost:4000";
 
 
 const api = axios.create({ baseURL: API_ROOT });
